5f0f0ef35055f4dedad4e8c812ead3fabd63cba2
Alireza-Nikzad <www.alireza2215@gmail.com>
2025-07-05 17:23:15 +0330

    Add numbered labels above detected faces
    - Used enumerate() to index each detected face
    - Displayed labels like "Face 1", "Face 2", etc. above bounding boxes
    - Improves UI by clearly identifying multiple faces

--- main.py
+++ main.py
@@ -29,12 +29,13 @@ while True:
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
     face_count = len(faces)
     
-    for (x, y, w, h) in faces:
+    for i, (x, y, w, h) in enumerate(faces, start= 1):
 
         face_roi = frame[y:y+h, x:x+w]
 
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(frame, "Face Detected", (x,y -10), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(0,255,0), 2)
+        cv2.putText(frame, f"Face {i}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.6, (0, 255, 0), 2)
         if mode == 'pixelate':
             small = cv2.resize(face_roi, (16, 16), interpolation=cv2.INTER_LINEAR)
             pixelated_face = cv2.resize(small, (w, h), interpolation=cv2.INTER_NEAREST)
